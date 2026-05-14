3853bc4a3841719dd565361f290821aa08edafe4
Aman king <ry650506@gmail.com>
2026-05-15 01:44:57 +0530

    Fix server start function by removing extra brace

--- server.ts
+++ server.ts
@@ -715,7 +715,6 @@ app.use((req, res, next) => {
   httpServer.listen(Number(PORT), "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
-}
 
 startServer().catch(err => {
   console.error("Failed to start server:", err);
